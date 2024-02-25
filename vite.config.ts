166a7f8787d80ac902688730c47763a4ab663eef
lathindu1 <alpdetails@gmail.com>
2024-02-25 21:42:12 +0530

    Vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/main.tsx', // Path to your library's entry file
+      name: 'ReacTailwindConfirmBox',
+      formats: ['es', 'cjs'], // Output both ES module and CommonJS formats
+      fileName: (format) => `react-tailwind-confirm-box.${format}.js`
+    },
+    rollupOptions: {
+      // Ensure external dependencies are not bundled into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Properly configure globals for UMD builds if needed
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
