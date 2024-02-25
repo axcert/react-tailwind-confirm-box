7745eece6e0814af88691f150a87deeab9c11dda
lathindu1 <alpdetails@gmail.com>
2024-02-25 16:20:39 +0530

    Type issues fixed

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,13 +1,25 @@
 // index.d.ts
 
+import { ReactNode } from "react";
+
+export enum ModalTheme {
+  DeleteConfirmation = "delete-confirmation",
+  ActionConfirmation = "action-confirmation",
+  CustomConfirmation = "custom-confirmation",
+}
+
 export interface ModalProps {
   isOpen: boolean;
+  theme?: ModalTheme;
   title: string;
   description: string;
   onConfirm: () => void;
   onCancel: () => void;
   confirmButtonText?: string;
   cancelButtonText?: string;
+  customContent?: ReactNode; // For CustomConfirmation theme
+  confirmButtonClasses?:string,
+  cancelButtonClasses?:string,
 }
 
 export declare function useConfirm(): {
